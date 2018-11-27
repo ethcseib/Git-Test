f970e56bbae8e405412e2c0926ec3b430f851e1b
Ethan Seiber <ethcseib@ut.utm.edu>
2018-11-27 04:07:16 -0600

    newly modified main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+int sum(int);
+
 int main(){
 
-  cout<< "Hello World!\n";
+  int n;
+
+  cout<< "Provide a number ";
+  cin>> n;
+
+  cout<< "The sum = " << sum(n) << endl;
+  
   return 0;
+}
+
+int sum(int n){
+
+  int sum = 0;
+  
+  for(int i = 1; i <= n; i++)
+    sum += i;
+
+  return sum;
 }
